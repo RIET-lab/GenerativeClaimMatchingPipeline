7a62f3e3dbea86bec5becdc99a354bc914ba8899
mshlis <mishliselberg@gmail.com>
2022-04-06 12:23:47 -0400

    fix dir mistake

--- aliases.sh
+++ aliases.sh
@@ -2,5 +2,5 @@ alias rerank-pretrain="python src/dynamicquery/cross_query/run_pretrain.py"
 alias rerank-train="python src/dynamicquery/cross_query/run_train.py"
 alias rerank-eval="python src/dynamicquery/cross_query/run_eval.py"
 
-alias selection-train="python src/dynamicquery/train_sentence_model.py"
+alias selection-train="python src/dynamicquery/candidate_selection/train_sentence_model.py"
 alias selection-eval=""
